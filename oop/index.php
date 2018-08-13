6ad2301d8aea479ed91b016f8bed93c5e22dea5f
dlugyp <dlugyp@googlemail.com>
2018-08-13 04:15:31 +0100

    Logout implemented and added OOP version

--- oop/index.php
+++ oop/index.php
@@ -5,13 +5,10 @@
 	// Require the config
 	require_once "inc/config.php"; 
 
-	ForceLogin();
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+  <head> 
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -26,7 +23,14 @@
   <body>
 
   	<div class="uk-section uk-container">
-  		Dashboard here; you are signed in as user: <?php echo $_SESSION['user_id']; ?>
+  		<?php 
+  			echo "Hello world. Today is: ";
+  			echo date("Y m d");
+  		?> 
+  		<p>
+  			<a href="/login.php">Login</a>
+  			<a href="/register.php">Register</a>
+  		</p>
   	</div>
 
   	<?php require_once "inc/footer.php"; ?> 
